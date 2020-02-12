230eadae3af77ad06c98f4efcc8f505b4e1455ba
paguilar <p.aguilar.2019@alumnos.urjc.es>
2020-02-12 18:18:34 +0100

    header

---
+++ session-04/body.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+filename = "U5.txt"
+
+# -- Open and read the file
+file_contents = Path(filename).read_text()
+
+# -- Print the contents on the console
+try:
+
+    with open (filename, "r") as f:
+        header=next(f)
+        for line in f:
+            components = line.replace('"', "").strip("\n").split(",")
+except FileNotFoundError:
+    print("file not found")
