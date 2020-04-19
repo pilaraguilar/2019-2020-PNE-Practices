c79bdb24df294c67d1dbac18f9f931a2118ae541
PILAR <p.aguilar.2019@alumnos.urjc.es>
2020-04-19 11:59:22 +0200

    p7

--- P7/EX03-gene-MIR633.py
+++ P7/EX03-gene-MIR633.py
@@ -1,6 +1,10 @@
+# in this exercise we program a client for connecting
+# to the sequence endpoint and we get the sequence and it description
+
 import http.client
 import json
 import termcolor
+
 
 GENES = {'FRAT1': 'ENSG00000165879', 'ADA': 'ENSG00000196839', 'FXN': 'ENSG00000165060', 'RNU6_269P': 'ENSG00000212379', 'MIR633': 'ENSG00000207552', 'TTTY4C': 'ENSG00000228296', 'RBMY2YP': 'ENSG00000227633', 'FGFR3': 'ENSG00000068078', 'KDR': 'ENSG00000128052', 'ANK2': 'ENSG00000145362',}
 
